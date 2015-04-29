e4a701051832bb9d69f8b6e264baf1136d41e4a1
rich <rtarro@us.ibm.com>
2015-04-28 22:08:21 -0400

    First commit

---
+++ demo.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var app = express();
+
+var Cloudant = require('cloudant')({account:'rich', key:'thishistakededoneentlydn',password:'8TbhuaocUNmg1ndWTBCT5h5I'});
+var db = Cloudant.use("animaldb");
+
+/*require the ibm_db module*/
+var ibmdb = require('ibm_db');
+
+// set up handlebars view engine
+var handlebars = require('express3-handlebars')
+         .create({defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+
+db.get("badger", function(err, data) {
+Cloudant_data = data.wiki_page;
+});
+
+ibmdb.open("DRIVER={DB2};DATABASE=sample;UID=db2inst1;PWD=password;HOSTNAME=DB-Ubuntu-VirtualBox;port=50000", function(err, conn)
+{
+        if(err) {
+                /*
+                  On error in connection, log the error message on console
+                */
+                console.error("error: ", err.message);
+        } else {
+                conn.query("select FIRSTNME, LASTNAME from employee where FIRSTNME='MICHAEL'", function(err, result) {       
+                        //console.log(result[0].FIRSTNME + ' ' + result[0].LASTNAME);
+                        DB2_data = (result[0].FIRSTNME + ' ' + result[0].LASTNAME);     
+                        conn.close(function(){
+                                console.log("Connection Closed");
+                        });
+                });
+        }
+});
+
+
+app.get('/', function(req, res){
+        res.render('home', { Cloudant_data: Cloudant_data, DB2_data: DB2_data });
+});
+
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+res.status(404);
+res.render('404');
+});
+
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+console.error(err.stack);
+res.status(500);
+res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log( 'Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate.' );
+});
